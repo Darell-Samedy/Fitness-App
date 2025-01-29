b8802a9503bf7773c2bbcfea74497dae7a3c9d39
Darell1103 <113571610+Darell1103@users.noreply.github.com>
2025-01-29 14:45:33 -0500

    Resolved merge conflicts

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // This tells Tailwind to purge unused styles from your React app
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
