0a54bba5f72e609eed3176536398f10282ce2a33
Darell1103 <113571610+Darell1103@users.noreply.github.com>
2025-02-12 20:28:50 -0500

    App.js code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
+import TopBar from "./scenes/global/topBar";
+import { ColorModeContext } from "./theme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import TopBar from "./scenes/global/topBar";
+
 function FitnessDashboard() {
+  const [theme, colorMode] = useMode();
+
+  return(
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className = "Content">
+            <TopBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
   return <div className="app"></div>;
 }
 
