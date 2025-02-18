ce73ca3d25519cd9f862eceeaf46d3513c527606
Ishan Patel <ishanpatel@ishans-air.mynetworksettings.com>
2025-02-17 22:24:33 -0500

    Updated App.js, added Header, modified dashboard and sidebar, added team page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ColorModeContext, useMode } from './theme';
 import TopBar from './scenes/global/TopBar';
 import SideBar from './scenes/global/SideBar';
 import Dashboard from './scenes/dashboard';
-// import Team from './scenes/team';
+import Team from './scenes/team';
 // import Invoices from './scenes/invoices';
 // import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
@@ -28,8 +28,8 @@ function App() {
             <TopBar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
-              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/team" element={<Team />} />
+              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/form" element={<Form />} />
@@ -37,7 +37,7 @@ function App() {
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/calendar" element={<Calendar />} /> */} 
             </Routes>
           </main>
         </div>
