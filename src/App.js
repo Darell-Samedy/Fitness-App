6909d1ba59be8beb8bbd6f3f23df9e1223d519cf
Darell1103 <113571610+Darell1103@users.noreply.github.com>
2025-02-24 18:06:47 -0500

    Finished up forms, and began adding Calendar section

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import TopBar from './scenes/global/TopBar';
 import SideBar from './scenes/global/SideBar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team/index.jsx';
-// import Invoices from './scenes/invoices';
-// import Contacts from './scenes/contacts';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts'; 
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -30,15 +30,15 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/bar" element={<Bar />} /> /*}
+              {/* <Route path="/form" element={<Form />} /> /*}
+              {/* <Route path="/line" element={<Line />} /> /*}
+              {/* <Route path="/pie" element={<Pie />} /> /*}
+              {/* <Route path="/faq" element={<FAQ />} /> /*}
+              {/* <Route path="/geography" element={<Geography />} /> /*}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
