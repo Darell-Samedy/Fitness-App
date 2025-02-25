85727743f3274b3771fa7af686f3be2f90873e47
Ishan Patel <ishanpatel@ishans-air.mynetworksettings.com>
2025-02-24 22:32:19 -0500

    Update App.js and add chart components and scene pages

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import Dashboard from './scenes/dashboard';
 import Team from './scenes/team/index.jsx';
 import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts'; 
-// import Bar from './scenes/bar';
-// import Form from './scenes/form';
-// import Line from './scenes/line';
-// import Pie from './scenes/pie';
-// import FAQ from './scenes/faq';
-// import Geography from './scenes/geography';
-// import Calendar from './scenes/calendar';
+import Bar from './scenes/bar';
+import Form from './scenes/form';
+import Line from './scenes/line';
+import Pie from './scenes/pie';
+import FAQ from './scenes/faq';
+import Geography from './scenes/geography';
+import Calendar from './scenes/calendar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -32,11 +32,11 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/bar" element={<Bar />} /> /*}
-              {/* <Route path="/form" element={<Form />} /> /*}
-              {/* <Route path="/line" element={<Line />} /> /*}
-              {/* <Route path="/pie" element={<Pie />} /> /*}
-              {/* <Route path="/faq" element={<FAQ />} /> /*}
+              <Route path="/bar" element={<Bar />} /> 
+              <Route path="/form" element={<Form />} /> 
+              <Route path="/line" element={<Line />} /> 
+              <Route path="/pie" element={<Pie />} /> 
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/geography" element={<Geography />} /> /*}
               {/* <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
